b0438c11db4d9ef146322d2ea3bfedd6714971f5
tomptrs <tom_ptrs@yahoo.com>
2015-09-01 15:03:16 +0200

    Project 2 code

---
+++ Project2/Deel 1/WorkOutApp/WorkOutApp/js/workout.js
@@ -0,0 +1,116 @@
+﻿/*
+Implementatie van de controller:
+1. Start workout
+2. show progress voor een oefening
+3. Na einde van tijdsduur toon volgende oefening
+4. Herhaal dit voor alle oefeningen
+*/
+
+/*
+    Deze klassen stellen het Model voor
+ */
+function Oefening(args) {
+    this.naam = args.naam;
+    this.titel = args.titel;
+    this.beschrijving = args.beschrijving;
+}
+
+
+function WorkOutPlan(args) {
+    this.naam = args.naam;
+    this.oefeningen = [];
+    this.titel = args.titel;
+    this.RustTussenOefeningen = args.RustTussenOefeningen;
+}
+
+//We betrekken de workout module uit app.js en registreren onze controller
+angular.module("workout").controller("WorkOutCtrl",['$scope', '$interval', function ($scope,$interval)
+{
+    var rustOefening;
+    var workoutPlan;
+   
+    var init = function ()
+    {
+       
+        console.log("init functie");
+        startWorkOut();
+    }
+
+    var startWorkOut = function ()
+    {
+        workoutPlan = CreateWorkOut();
+        console.log(workoutPlan);
+        rustOefening =
+            {
+                details: new Oefening({
+                    naam: "Rust",
+                    beschrijving: "Rust een beetje",
+                    titel: "Rust!"
+                }),
+                duration: 10
+            };
+
+        
+        //Start Oefening
+        startOefeningen(workoutPlan.oefeningen[0]);
+    }
+
+    var CreateWorkOut = function ()
+    {
+        console.log("create workout");
+        var workout = new WorkOutPlan(
+            {
+                naam: "5min workout",
+                titel : "5 minute workout",
+                RustTussenOefeningen: 10
+            });
+
+        workout.oefeningen.push(
+            {
+                details: new Oefening(
+                    {
+                        naam: "oef 1 springen",
+                        titel: "oef 1: springen",
+                        beschrijving: "spring constant"
+                    }),
+                duration: 30
+            },
+            
+             {
+                 details: new Oefening(
+                     {
+                         naam: "oef 2 pompen",
+                         titel: "oef 2: pompen",
+                         beschrijving: "pomp constant"
+                     }),
+                 duration: 30
+             });
+        console.log(workout);
+        return workout;
+    }
+
+
+    /*
+    De $interval service is een wrapper over de window.setInterval methode. Hoofddoel is om een 
+    specifieke functie steeds weer op te roepen met een bepaald tijdsinterval.
+    Door de interval op te roepen geven we een callback functie mee (als anonieme functie)
+    We incrementeren tot de duration per oefening zodat de $interval automatisch stopt, anders blijft deze 
+    oneindig doorlopen
+    */
+    var startOefeningen = function (oefening) {
+        console.log("start oefeningen")
+
+        $scope.huidigeOefening = oefening;
+        $scope.huidigOefeningDuration = 0;
+        console.log($scope.huidigeOefening);
+
+        $interval(function () {
+            console.log($scope.huidigOefeningDuration);
+            $scope.huidigOefeningDuration++;
+        },
+        1000,
+        $scope.huidigeOefening.duration);
+    };
+
+    init();
+}]);
---
+++ Project2/README.md
@@ -0,0 +1,25 @@
+# Project 2: WorkOut App
+
+## Requirements
+
+Deze workout app is een planning voor fitness oefeningen om binnen de 5 minuten 10 oefeningen achtereenvolgens uit te 
+voeren. Dus we moeten 10 oefeningen voorzien waarbij elke oefening 30 seconden duurt, gevolgd door een rust periode. Na
+de rust wordt automatisch de volgende oefening gestart.
+
+* De workout starten
+* Voorzie step-by-step instructies hoe men de oefening moet uitvoeren
+* Een timer om te kijken hoeveel tijd nog rest voor de oefening
+* Notificatie aan de gebruiker wanneer de oefening gedaan is
+* De mogelijkheid om de oefening te pauzeren
+* Informatie voor de volgende oefening voorzien
+
+Om de juiste taal te spreken is een workout een serie oefeningen uitgevoerd in een bepaalde volgorde met een bepaalde 
+tijdsduur.
+
+Een oefening heeft:
+* Naam
+* Titel (aan de gebruiker getoond)
+* beschrijving
+* instructies
+
+
