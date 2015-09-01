3fab02d5eab201f24aa329b2debc46d417b431c2
tomptrs <tom_ptrs@yahoo.com>
2015-09-01 10:50:10 +0200

    Code

---
+++ Project1/WebApplication1/app.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module("app", []);
+
+app.controller("GuessTheNumberCtrl", function ($scope)
+{
+    $scope.verifyGuess = function ()
+    {
+        $scope.afwijking = $scope.origineel - $scope.guess;
+        $scope.aantalPogingen = $scope.aantalPogingen + 1;
+    }
+
+    $scope.initialize = function ()
+    {
+        $scope.aantalPogingen = 0;
+        $scope.origineel = Math.floor(Math.random() * 1000) + 1;
+        $scope.guess = null;
+        $scope.afwijking = null;
+    }
+
+    $scope.initialize();
+});
